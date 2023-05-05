ef14c4e528649f472029d737a9d9dcddd373786f
mitternachtsmond <cse200001056@iiti.ac.in>
2023-05-05 20:34:21 +0530

    final_client

--- client.py
+++ client.py
@@ -3,17 +3,19 @@ from ast import literal_eval
 import json
 from utils import cost
 
-# main_url = "http://192.168.145.165:8080"
-main_url = "http://127.0.0.1:8080"
+main_url = "http://10.250.3.163:8080"
+# main_url = "http://127.0.0.1:8080"
 
-
-# host1:host2: host1->S1->S2->host2
-def display_paths(nodeList):
-    path = []
-    for node in nodeList:
-        path.append(node)
-    print("->".join(path))
-
+# host1:host2 :host1->S1->S2->host2
+def display_paths(paths):
+    for key, value in paths.items():
+        if len(value) < 2:
+            continue
+        if key[0] == "h":
+            print(f"{value[0]}-{key} :", end="")
+            print("->".join(value))
+    print()
+    
 
 # in_port,dl_src,dl_dst actions=output
 def display_rules(rules):
@@ -30,6 +32,8 @@ def display_rules(rules):
 
 # <node1-node2> : <cost>
 def display_links(links):
+    print("\n***************************************\n")
+    print("Netwrok Links and Cost\n")
     nodes_done = {}
     for k, v in links.items():
         if nodes_done.get(k[0] + k[1], 0) == 1:
@@ -40,49 +44,46 @@ def display_links(links):
 
 
 # Add a new connection between two hosts with a given bandwidth
-def add_connection():
+def add_connection(n):
     url = main_url + "/add"
-    print("1 MAC Based\n2 IP Based\n3 Name Based")
-    n = input("\nEnter your choice: ")
+    print("Enter host1 host2 bandwidth")
+    inp_str = input()
+    inp_list = inp_str.split()
+    if len(inp_list) < 3:
+        print("Invalid Input...\nTry Again!\n")
+        return
     data = {
         "id_spec": "mac" if n == "1" else "ip" if n == "2" else "",
         "src": "",
         "dst": "",
         "bw": 0,
     }
-    while True:
-        print("Enter host1 host2 bandwidth")
-        inp_str = input()
-        if inp_str == "exit":
-            break
-        inp_list = inp_str.split()
-        if len(inp_list) < 3:
-            print("Invalid Input...\nTry Again!\n")
-            continue
-        host1 = inp_list[0]
-        host2 = inp_list[1]
-        try:
-            bw = float(inp_list[2])
-        except ValueError:
-            print("Invalid Input... Bandwidth must be a number\nTry Again!\n")
-            continue
-        data["src"] = host1
-        data["dst"] = host2
-        data["bw"] = bw
-        try:
-            response = requests.post(url, json=data)
-        except:
-            print("Connection Refused... Try Again Later!\n")
-            exit(0)
+    host1 = inp_list[0]
+    host2 = inp_list[1]
+    try:
+        bw = float(inp_list[2])
+    except ValueError:
+        print("Invalid Input... Bandwidth must be a number\nTry Again!\n")
+        return
+    data["src"] = host1
+    data["dst"] = host2
+    data["bw"] = bw
+    try:
+        response = requests.post(url, json=data)
+    except:
+        print("Connection Refused... Try Again Later!\n")
+        exit(0)
 
-        res = response.json()
+    res = response.json()
 
-        print("\nUpdated:\n")
-        display_paths(res["path"])
-        display_rules(res["rules"])
-        print("\nOriginal:\n")
-        display_paths(res["orig_path"])
-        display_rules(res["orig_rules"])
+    print("\nUpdated:\n")
+    print("->".join(res["path"]))
+    # display_paths(res["path"])
+    display_rules(res["rules"])
+    print("\nOriginal:\n")
+    print("->".join(res["orig_path"]))
+    # display_paths(res["orig_path"])
+    display_rules(res["orig_rules"])
 
 
 # Get all links and their costs
@@ -101,34 +102,66 @@ def get_links():
         print(e)
         exit(0)
 
-
 # Minimum cost from a particular host to all other hosts
 def getMinPath():
-    while True:
-        host = input("Enter host name: ")
-        url = f"{main_url}/paths/{host}"
-        try:
-            response = requests.get(url)
-        except requests.exceptions.ConnectionError:
-            print("Connection Refused... Try Again Later!\n")
-            exit(0)
-        res = response.json()
-        display_paths(res)
-
-
+    host = input("Enter host name: ")
+    url = f"{main_url}/paths/{host}"
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print("Connection Refused... Try Again Later!\n")
+        exit(0)
+    res = response.json()
+    display_paths(res)
+        
 # Get rules for a particular switch
 def get_rules():
     switch_num = int(input("Enter switch number: "))
     url = f"{main_url}/flows/{switch_num}"
     try:
         res = requests.get(url).json()
+        print(res)
     except requests.exceptions.ConnectionError:
         print("Connection Refused... Try Again Later!\n")
         exit(0)
 
 
-# getMinPath()
-add_connection()
-# links=get_links()
-# print(links)
-# get_rules()
+
+def main():
+    get_links()
+    
+    
+    while True:
+        t=input("\n1 Get rules for a switch\n2 Get minimum path\n3 Add Connections\n4 Exit\nEnter your choice: ")
+        if t=='1':
+            get_rules()
+        elif t=='2':
+            while 1:
+                getMinPath()
+                while 1:
+                    choice = input("Do you want to continue(y/n): ")
+                    if choice not in ['y','n','Y','N']: print("Invalid Choice!")
+                    else: break
+                if choice.lower() == 'n':
+                    break
+        elif t=='3':
+                print("\n1 MAC Based\n2 IP Based\n3 Name Based")
+                n = input("\nEnter your choice: ")
+                while 1:
+                    add_connection(n)
+                    while 1:
+                        choice = input("Do you want to continue(y/n): ")
+                        if choice not in ['y','n','Y','N']: print("Invalid Choice!")
+                        else: break
+                    if choice.lower() == 'n':
+                        break
+        elif t=='4':
+            
+            exit(0)
+        else:
+            print("Invalid Choice\n")
+
+
+
+if __name__=='__main__':
+    main()
